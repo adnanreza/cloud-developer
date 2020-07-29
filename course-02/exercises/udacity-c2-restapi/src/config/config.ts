5cc7b6ba37c55b74da3a8fa121d50dd29de45ef1
Adnan Reza <adnan.reza@outlook.com>
2020-07-29 09:55:50 -0700

    Added get feedItem by id route

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: 'adnanreza',
+    password: '12rrAR88',
+    database: 'udagramAR',
+    host: 'udacityc2.ck7fzwxmzpyq.ca-central-1.rds.amazonaws.com',
+    dialect: 'postgres',
+    aws_region: 'ca-central-1',
+    aws_profile: 'default',
+    aws_media_bucket: 'udagram-ar-dev',
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: '',
+    password: '',
+    database: 'udagram_prod',
+    host: '',
+    dialect: 'postgres',
+  },
+};
